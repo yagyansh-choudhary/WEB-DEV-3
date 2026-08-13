f86951519fa4d9b130bde4d56dbd14cd7cd6980c
kali <kali@example.com>
2026-08-13 20:39:08 +0530

    latest

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require ('express');
-
 const app = express();
+
+const packages = require("./tour");
 
 app.get('/',(req,res)=>{
     res.send("hello world");
-})
+});
 
+app.get("/packages",(req,res)=>{
+    res.json(packages);
+});
+
+app.get("/pakages/:id",(req,res)=>{
+    const packageId = parsInt(req.params.id);
+    const selectedpackage = packages.find(item => item.id === packageId);
+    res.json(selectedpackage);
+});
 app.listen(3000,()=>{
     console.log("server is running on 3000");
-})
+});                                                       
