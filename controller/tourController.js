73c0934168cc07c4a23ee6bd0136206e97c97aa9
kali <kali@example.com>
2026-09-03 20:50:25 +0530

    Add code for 3/9/26

--- controller/tourController.js
+++ controller/tourController.js
@@ -9,32 +9,42 @@ const getAllTours = (req, res) => {
 // Get a single tour by ID
 const getTourById = (req, res) => {
   const tour = tourModel.getById(req.params.id);
+
   if (!tour) {
     return res.status(404).json({ message: "Tour not found" });
   }
+
   res.json(tour);
 };
 
+// Get tours by query
 const getTourByquery = (req, res) => {
   const query = req.query.name;
   const tours = tourModel.getByquery(query);
   res.json(tours);
 };
 
+// Save tours
 const saveTours = (req, res) => {
   const tours = req.body;
   tourModel.save(tours);
   res.status(201).json(tours);
-
-  const updateTours = (id, updatedTour) => {
-    const id = req.params.id;
-    const data = req.body;
-    tourModel.updateTour(id);
-  };
 };
+
+// Update tour
+const updateTours = (req, res) => {
+  const id = req.params.id;
+  const data = req.body;
+
+  tourModel.updateTour(id, data);
+
+  res.json({ message: "Tour updated successfully" });
+};
+
 module.exports = {
   getAllTours,
   getTourById,
   getTourByquery,
   saveTours,
+  updateTours,
 };
--- index.js
+++ index.js
@@ -1,27 +1,93 @@
+// // after calling a function, it returns an object it is called factory function
 // const express = require('express');
-
 // const app = express();
+
 // const packages = require('./data/tour');
-// app.get("/", (req, res) => {
-//     res.send("Hello, World!");
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World');
 // });
 
-// app.get("/packages", (req, res) => {
-//   const packageId = parseInt(req.params.id);
-//   const package = packages.find(pkg => pkg.id === packageId);
+// app.get('/packages', (req, res) => {
+//     res.json(packages);
+// });
+
+// // use http://localhost:3000/packages in postman to get all the packages --------------------------------
+
+// app.get('/packages/:id', (req, res) => {
+//     const packageId = parseInt(req.params.id);
+//     const selectedPackage = packages.find(pkg => pkg.id === packageId); // comparing what id we put in postman with the id in the packages array, if true then it will return the package with that id
 //     res.json(selectedPackage);
 // });
+// // use http://localhost:3000/packages/1 in postman to get the package with id 1 here
+// // --------------------------------
+
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+// // query parameters are used to filter the data based on certain criteria. In this case, we can use query parameters to filter the packages based on the destination. For example, if we want to get all the packages that have a destination of "Maldives", we can use the following URL: http://localhost:3000/packages?des=Maldives
+// // http://localhost:3000/packages?des=maldieves
+
+// const express = require("express");
+
+// const app = express();
+
+// const packages = require("./routes/tourRoutes");
+
+// app.use(express.json());
+
+// app.use("/api", tourRoutes);
 
 // app.listen(3000, () => {
-//     console.log("Server is running on port 3000");
+//   console.log("Server is running on port 3000");
+// });
+
+// app.get("/",(req, res)=>{
+//   res.send("Hello World!");
+// });
+
+// app.get("/packages",(req, res)=>{
+//   res.json(packages);
+// });
+
+// // app.get("/packages/:id",(req, res)=>{
+// //   const packageId = parseInt(req.params.id);
+// // });
+
+// app.get("/packages/:id",(req, res)=>{
+//   const packageId = parseInt(req.params.id);
+//   const selectedPackage = packages.find(item => item.id === packageId);
+//   res.json(selectedPackage);
+// });
+
+// app.listen(3000,()=>{
+//   console.log("Server is running on 3000");
 // });
 
 const express = require("express");
+
 const app = express();
-const tourRoutes = require("./routes/tourRoutes");
+
+// const tourRoutes = require("./routes/tourRoutes");
 
 app.use(express.json());
-app.use("/api", tourRoutes);
+
+// app.use("/api", tourRoutes);
+
+// const middleware = (req, res, next) => {
+//   console.log("Middleware executed");
+//   next();
+// }
+
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+app.use(logger);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
